16d1b27326a0fe0e50379a75daee73f19ae28db1
Artem <artem.shumidub@gmail.com>
2019-08-16 08:54:55 +0300

    Update Launch.kt

--- Launch.kt
+++ Launch.kt
@@ -1,3 +1,12 @@
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+fun main(args: Array<String>) {
+
+//sampleStart
 println("Start")
 GlobalScope.launch(Dispatchers.Default) {
   println("Launch coroutine")
@@ -12,3 +21,8 @@ suspend fun longComputation() : Int{
   delay(2000L)
   return 42
 }
+//sampleEnd                 
+
+}
+
+
