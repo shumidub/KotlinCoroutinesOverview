68920b2a0c34cbcdfb82df019f6c26a8102e7b4f
Artem <artem.shumidub@gmail.com>
2019-08-19 10:31:40 +0300

    Create Flow.kt

---
+++ Flow.kt
@@ -0,0 +1,23 @@
+// kotlin std lib 
+// package kotlin.sequences
+// For Builds a sequence lazily yielding values one by one
+// On same thread
+
+suspend fun SequenceScope<Int>.printAndYield(result : Int){
+  println("Sent: $result")
+  yield(result)
+}
+
+val fibonacci : Sequence<Int> = sequence {
+    printAndYield(1) // first Fibonacci number
+    var current = 1
+    var next = 1
+    while (true) {
+        printAndYield(next) // next Fibonacci number
+        val tmp = current + next
+        current = next
+        next = tmp
+    }
+}
+
+fibonacci.take(10).forEach { println("Received: $it") }
